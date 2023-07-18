76a8d5a9a85c41f6bdfc06f19b886fe2c630cac2
Malar Sivakumar <malaraustin1@gmail.com>
2023-07-18 11:04:09 -0500

    Complete iteration 0.

--- ToDoListt/ContentView.swift
+++ ToDoListt/ContentView.swift
@@ -10,14 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+                  HStack {
+                      Text("To Do List")
+                          .font(.system(size:40))
+                          .fontWeight(.black)
+                      Spacer()
+                      Button(action:
+                              {                }){
+                          Text("+")
+                      }
+                      }
+              
+              }.padding()
+              }
+          }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ ToDoListt/NewToDoView.swift
@@ -0,0 +1,39 @@
+//
+//  NewToDoView.swift
+//  ToDoListt
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    
+    @State var title: String
+    @State var isImportant: Bool
+    
+    var body: some View {
+        VStack {
+            Text("Task title: ")
+            TextField("Enter the task description...", text: $title)
+            
+            Toggle(isOn: $isImportant){
+                Text("Is it important?")
+                
+                Button(action: {
+                    
+                }){
+                    Text("Add")
+                }.padding()
+                
+            }.padding()
+        }
+        
+    }
+}
+
+struct NewToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewToDoView(title:"", isImportant: false)
+    }
+}
