a4bfdf2dda810104e192a5c49f6ca0f3021a9621
Malar Sivakumar <malaraustin1@gmail.com>
2023-07-18 10:29:29 -0500

    Initial Commit

---
+++ ToDoListt/ToDoListtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListtApp.swift
+//  ToDoListt
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
